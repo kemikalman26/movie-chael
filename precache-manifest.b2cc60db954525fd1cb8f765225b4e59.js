e7b0b58b3753538b5ccd5424b273e5117dff1e63
unknown <michael.photos26@gmail.com>
2022-12-01 16:21:47 +0800

    Updates

---
+++ precache-manifest.b2cc60db954525fd1cb8f765225b4e59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c58f3ac7ef3879d1bca",
+    "url": "/movie-chael/static/css/main.c40e19e9.chunk.css"
+  },
+  {
+    "revision": "5c58f3ac7ef3879d1bca",
+    "url": "/movie-chael/static/js/main.5c58f3ac.chunk.js"
+  },
+  {
+    "revision": "8ea8bb999b24eb958400",
+    "url": "/movie-chael/static/js/1.8ea8bb99.chunk.js"
+  },
+  {
+    "revision": "1cba59cddeb091067b0f",
+    "url": "/movie-chael/static/js/runtime~main.1cba59cd.js"
+  },
+  {
+    "revision": "b98b9440cdb944acae3b80ee58cdbfe7",
+    "url": "/movie-chael/static/media/search.b98b9440.svg"
+  },
+  {
+    "revision": "29b9eba2b70d7288510f6a65ac70c547",
+    "url": "/movie-chael/index.html"
+  }
+];
